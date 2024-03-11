f28d590aee2ac126324360b1d94a93d6afd946b2
owendenied <157000201+owendenied@users.noreply.github.com>
2024-03-11 10:50:05 +0800

    Add files via upload

---
+++ scripts/music-list.js
@@ -0,0 +1,295 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Come Closer",
+    artist: "marsquake - Satyr",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Meditation",
+    artist: "marsquake - Satyr",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Circle of Friends",
+    artist: "marsquake - Satyr",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Contenance ft. Danæ",
+    artist: "marsquake - Satyr",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Dream Chaser",
+    artist: "marsquake - Satyr",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "After Sun ft. Phlocalyst",
+    artist: "marsquake - Satyr",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Little Spoon ft. No Spirit ",
+    artist: "marsquake - Satyr",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Poles Apart",
+    artist: "slowburn",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Lightleaks",
+    artist: "slowburn",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Callisto",
+    artist: "slowburn",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "A Second Worth",
+    artist: "slowburn",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Midspring",
+    artist: "slowburn",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Alissa",
+    artist: "slowburn",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "As It Was",
+    artist: "slowburn",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "New Light",
+    artist: "slowburn",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Between Time",
+    artist: "slowburn",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Carousel",
+    artist: "Kupla",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Giants",
+    artist: "Kupla",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Noir",
+    artist: "Kupla",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Endurance",
+    artist: "Kupla",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "It’s Like You Never Left ft. less.people ",
+    artist: "Kupla",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Elsewhere",
+    artist: "Kupla",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Slumber Song",
+    artist: "Kupla",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "End of the Road",
+    artist: "Kupla",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "shimmer",
+    artist: "dontcry - Bcalm",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "bloom",
+    artist: "dontcry - Bcalm",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "nightwatch",
+    artist: "dontcry - Bcalm",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "adrift",
+    artist: "dontcry - Bcalm",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "searching",
+    artist: "dontcry - Bcalm",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "raindrops",
+    artist: "dontcry - Bcalm",
+    img: "music-30",
+    src: "music-30"
+  },
+  {
+    name: "passage",
+    artist: "dontcry - Bcalm",
+    img: "music-31",
+    src: "music-31"
+  },
+  {
+    name: "night and day",
+    artist: "dontcry - Bcalm",
+    img: "music-32",
+    src: "music-32"
+  },
+  {
+    name: "First Encounter",
+    artist: "swink",
+    img: "music-33",
+    src: "music-33"
+  },
+  {
+    name: "In Front Of Me",
+    artist: "swink",
+    img: "music-34",
+    src: "music-34"
+  },
+  {
+    name: "My Dear",
+    artist: "swink",
+    img: "music-35",
+    src: "music-35"
+  },
+  {
+    name: "When The Winters Come",
+    artist: "swink",
+    img: "music-36",
+    src: "music-36"
+  },
+  {
+    name: "Go Back In Time",
+    artist: "swink",
+    img: "music-37",
+    src: "music-37"
+  },
+  {
+    name: "To Better Come Back",
+    artist: "swink",
+    img: "music-38",
+    src: "music-38"
+  },
+  {
+    name: "Rainy Day",
+    artist: "Two Scents",
+    img: "music-39",
+    src: "music-39"
+  },
+  {
+    name: "Only You",
+    artist: "Two Scents",
+    img: "music-40",
+    src: "music-40"
+  },
+  {
+    name: "Simple Things",
+    artist: "Two Scents",
+    img: "music-41",
+    src: "music-41"
+  },
+  {
+    name: "Evergreen",
+    artist: "Two Scents",
+    img: "music-42",
+    src: "music-42"
+  },
+  {
+    name: "Old Friends",
+    artist: "Two Scents",
+    img: "music-43",
+    src: "music-43"
+  },
+  {
+    name: "Silver Moon",
+    artist: "Two Scents",
+    img: "music-44",
+    src: "music-44"
+  },
+  {
+    name: "Fading",
+    artist: "Two Scents",
+    img: "music-45",
+    src: "music-45"
+  },
+
+
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
